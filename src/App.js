9d7f2319f9eba3821de46ca5c42fb8e23e14392e
bipinstha7 <bipin.shrestha901@gmail.com>
2018-06-14 23:36:49 +0545

    add profile modal

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
 import Signin from "./components/Signin/Signin";
 import Register from "./components/Register/Register";
+import Modal from "./components/Modal/Modal";
+import Profile from "./components/Profile/Profile";
+
 import './App.css';
 
 // import Particles from 'react-particles-js';
@@ -29,6 +32,7 @@ const initialState = {
   boxes: [],
   route: "home",
   isSignedIn: true,
+  isProfileOpen: false,
   user: {
     id: "",
     name: "",
@@ -121,12 +125,22 @@ class App extends Component {
     this.setState({route: route});
   }
 
+  toggleModal = () => {
+    this.setState(prevState => ({
+      ...prevState,
+      isProfileOpen: !prevState.isProfileOpen}));
+  }
+
   render() {
-    const { isSignedIn, imageUrl, route, boxes} = this.state;
+    const { isSignedIn, imageUrl, route, boxes, isProfileOpen} = this.state;
     return (
       <div className="App"> 
         {/* <Particles className="particles" params={particlesOptions} /> */}
-        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} toggleModal={this.toggleModal}/>
+        {isProfileOpen && 
+          <Modal>
+            <Profile isProfileOpen={isProfileOpen} toggleModal={this.toggleModal}/>
+          </Modal>}
         {route === "home" 
           ? <div>
               <Logo />
--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -28,7 +28,7 @@ class ProfileIcon extends React.Component {
             <img src="http://tachyons.io/img/logo.jpg" alt="avatar" className="br-100 ba h3 w3 dib" />
           </DropdownToggle>
           <DropdownMenu right className="b--transparent shadow-5" style={{marginTop: '20px', backgroundColor: 'rgba(255,255,255,0.5'}}>
-            <DropdownItem>View Profile</DropdownItem>
+            <DropdownItem onClick={this.props.toggleModal}>View Profile</DropdownItem>
             <DropdownItem onClick={() => this.props.onRouteChange("signout")}>Sign out</DropdownItem>
           </DropdownMenu>
         </Dropdown>
