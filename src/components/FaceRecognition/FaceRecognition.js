1307406c5a7d1772501a5d5911ebba29a09c1dc7
bipinstha7 <bipin.shrestha901@gmail.com>
2019-04-07 08:16:37 +0545

    show boxes around the face of the people in the image

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,19 +1,28 @@
-import React from "react";
-import "./FaceRecognition.css";
+import React from 'react'
+import './FaceRecognition.css'
 
-const FaceRecognition = ({imageUrl, boxes}) => {
-  return (
-    <div className="center">
-      <div className="mt2 absolute">
-        <img src= {imageUrl} alt="" id="inputImage" width="500px" />
-        { boxes.map(box => {
-          return <div key={box.topRow} className="bounding-box" style={{ top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}>
-          </div>
-        })
-      }
-      </div>
-    </div>
-  )
+const FaceRecognition = ({ imageUrl, boxes }) => {
+	return (
+		<div className="center">
+			<div className="mt2 absolute">
+				<img src={imageUrl} alt="" id="inputImage" width="500px" />
+				{boxes.map(box => {
+					return (
+						<div
+							key={box.topRow}
+							className="bounding-box"
+							style={{
+								top: box.topRow,
+								right: box.rightCol,
+								bottom: box.bottomRow,
+								left: box.leftCol
+							}}
+						/>
+					)
+				})}
+			</div>
+		</div>
+	)
 }
 
-export default FaceRecognition;
+export default FaceRecognition
