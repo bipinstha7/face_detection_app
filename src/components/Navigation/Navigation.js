a0f43dbe39c208343d14bd833403722bd1141fc8
bipinstha7 <bipin.shrestha901@gmail.com>
2019-04-11 19:56:28 +0545

    sign in and register navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,21 +1,25 @@
-import React from "react";
-import ProfileIcon from "../Profile/ProfileIcon";
+import React from 'react'
+import ProfileIcon from '../Profile/ProfileIcon'
 
 const Navigation = ({ onRouteChange, isSignedIn, toggleModal }) => {
-  if (isSignedIn) {
-    return (
-      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
-        <ProfileIcon onRouteChange={onRouteChange} toggleModal={toggleModal}/>
-      </nav>
-    )
-  } else {
-    return (
-      <nav style={{ display: "flex", justifyContent: "flex-end" }}>
-        <p className="f4 dim pr5 pointer" onClick={() => onRouteChange("signin")}>Sign in</p>
-        <p className="f4 dim pr5 pointer" onClick={() => onRouteChange("register")}>Register</p>
-      </nav>
-    )
-  }
+	if (isSignedIn) {
+		return (
+			<nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+				<ProfileIcon onRouteChange={onRouteChange} toggleModal={toggleModal} />
+			</nav>
+		)
+	} else {
+		return (
+			<nav style={{ display: 'flex', justifyContent: 'flex-end' }}>
+				<p className="f4 dim pr5 pointer" onClick={() => onRouteChange('signin')}>
+					Sign in
+				</p>
+				<p className="f4 dim pr5 pointer" onClick={() => onRouteChange('register')}>
+					Register
+				</p>
+			</nav>
+		)
+	}
 }
 
-export default Navigation;
+export default Navigation
