8f03481a3b80e0153a0a840741c616a31889ff19
bipinstha7 <bipin.shrestha901@gmail.com>
2019-04-12 20:08:07 +0545

    user icon

--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -1,40 +1,46 @@
-import React from "react";
-import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import React from 'react'
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap'
 
 class ProfileIcon extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dropdownOpen: false
-    }
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			dropdownOpen: false
+		}
+	}
 
-  toggle = () => {
-    this.setState(prevState => ({
-      dropdownOpen: !prevState.dropdownOpen
-    }));
-    // this.setState({dropdownOpen: !this.state.dropdownOpen});
-  }
+	toggle = () => {
+		this.setState(prevState => ({
+			dropdownOpen: !prevState.dropdownOpen
+		}))
+		// this.setState({dropdownOpen: !this.state.dropdownOpen});
+	}
 
-  render() {
-    return (
-      <div className="pa4 tc">
-        <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
-          <DropdownToggle
-            tag="span"
-            data-toggle="dropdown"
-            aria-expanded={this.state.dropdownOpen}
-          >
-            <img src="http://tachyons.io/img/logo.jpg" alt="avatar" className="br-100 ba h3 w3 dib" />
-          </DropdownToggle>
-          <DropdownMenu right className="b--transparent shadow-5" style={{marginTop: '20px', backgroundColor: 'rgba(255,255,255,0.5'}}>
-            <DropdownItem onClick={this.props.toggleModal}>View Profile</DropdownItem>
-            <DropdownItem onClick={() => this.props.onRouteChange("signout")}>Sign out</DropdownItem>
-          </DropdownMenu>
-        </Dropdown>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="pa4 tc">
+				<Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+					<DropdownToggle tag="span" data-toggle="dropdown" aria-expanded={this.state.dropdownOpen}>
+						<img
+							src="http://tachyons.io/img/logo.jpg"
+							alt="avatar"
+							className="br-100 ba h3 w3 dib"
+						/>
+					</DropdownToggle>
+					<DropdownMenu
+						right
+						className="b--transparent shadow-5"
+						style={{ marginTop: '20px', backgroundColor: 'rgba(255,255,255,0.5' }}
+					>
+						<DropdownItem onClick={this.props.toggleModal}>View Profile</DropdownItem>
+						<DropdownItem onClick={() => this.props.onRouteChange('signout')}>
+							Sign out
+						</DropdownItem>
+					</DropdownMenu>
+				</Dropdown>
+			</div>
+		)
+	}
 }
 
-export default ProfileIcon;
+export default ProfileIcon
