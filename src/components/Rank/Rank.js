e0f105fea9221171a4aa580e5eb5774adcd3e854
bipinstha7 <bipin.shrestha901@gmail.com>
2019-04-13 16:31:17 +0545

    User rank by uploading images

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,12 @@
-import React from "react";
+import React from 'react'
 
-const Rank = ({name, entries}) => {
-  return (
-    <div>
-      <div className="f3 white">
-        {`${name.toUpperCase()}, your current entry count is...`}
-      </div>
-      <div className="f1 white">
-        {entries}
-      </div>
-    </div>
-  )
+const Rank = ({ name, entries }) => {
+	return (
+		<div>
+			<div className="f3 white">{`${name.toUpperCase()}, your current entry count is...`}</div>
+			<div className="f1 white">{entries}</div>
+		</div>
+	)
 }
 
-export default Rank;
+export default Rank
